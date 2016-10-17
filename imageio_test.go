c9a1058a17bab4bdcb07a4241a1b5fe370111b11
stanliski <1015757334@qq.com>
2016-10-17 17:33:16 +0800

    add test case

--- imageio_test.go
+++ imageio_test.go
@@ -3,14 +3,33 @@ package imageio
 import (
 	"log"
 	"testing"
+	"time"
+	"fmt"
 )
 
+func TestWritePNGImageTime(t *testing.T) {
+	start := time.Now()
+	mp4 := NewMp4("test.mp4", &Options{FPS:24})
+	mp4.WriteImage("images/test.jpg")
+	fmt.Println("cost: ", time.Now().Sub(start).Seconds())
+}
+
+func TestWriteJPGImageTime(t *testing.T) {
+	start := time.Now()
+	mp4 := NewMp4("test.mp4", &Options{FPS:24})
+	mp4.WriteImage("images/camera.png")
+	fmt.Println("cost: ", time.Now().Sub(start).Seconds())
+}
+
 func TestWriteImage(t *testing.T) {
-	mp4 := NewMp4("test.mp4", &Options{})
+	start := time.Now()
+	mp4 := NewMp4("test.mp4", &Options{FPS:24})
 	for i := 0; i < 100; i++ {
-		err := mp4.WriteImage("images/camera.png")
-		if err != nil {
-			log.Fatal(err)
+		err1 := mp4.WriteImage("images/camera.png")
+		err2 := mp4.WriteImage("images/test.jpg")
+		if err1 != nil && err2 != nil {
+			log.Fatal(err1)
 		}
 	}
+	fmt.Println("cost: ", time.Now().Sub(start).Seconds())
 }
